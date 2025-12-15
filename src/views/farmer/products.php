5e00a63747b9d4892ac8474c310666550f8b74e5
lulit-gif <lulittadesse789@gmail.com>
2025-12-15 23:33:36 +0300

    update description

--- src/views/farmer/products.php
+++ src/views/farmer/products.php
@@ -3,7 +3,7 @@
 <h1>Your Products</h1>
 <p>Listing of products you manage as a farmer.</p>
 <div class="product-grid">
-	<!-- farmer products will be rendered here -->
+	<!-- farmer products will be listed here -->
 </div>
 
 <?php require __DIR__ . '/../layout/footer.php'; ?>
