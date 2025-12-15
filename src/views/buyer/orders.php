c5d35c55643185d1fb45cc4e7f386c91f28a40c4
lulit-gif <lulittadesse789@gmail.com>
2025-12-15 23:20:05 +0300

    update order page

--- src/views/buyer/orders.php
+++ src/views/buyer/orders.php
@@ -1,6 +1,6 @@
 <?php require __DIR__ . '/../layout/header.php'; ?>
 
 <h1>Your Orders</h1>
-<p>List of past orders and their statuses.</p>
+<p>List of past orders and their status.</p>
 
 <?php require __DIR__ . '/../layout/footer.php'; ?>
