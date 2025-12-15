331e65ada5af25cde8a3813e80abb5aa945c6694
lulit-gif <lulittadesse789@gmail.com>
2025-12-15 23:31:43 +0300

    update low stock warning

--- src/views/farmer/low-stock-warning.php
+++ src/views/farmer/low-stock-warning.php
@@ -1,6 +1,6 @@
 <?php require __DIR__ . '/../layout/header.php'; ?>
 
-<h1>Low Stock Warning</h1>
+<h1>Low Stock warning</h1>
 <p>Products with low stock will be highlighted here.</p>
 
 <?php require __DIR__ . '/../layout/footer.php'; ?>
