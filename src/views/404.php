b10211c6e842650a8402299c6a4813b057724e7b
lulit-gif <lulittadesse789@gmail.com>
2025-12-17 12:33:31 +0300

    deleted the 404 not found file

--- src/views/404.php
+++
@@ -1,7 +0,0 @@
-<?php require __DIR__ . '/layout/header.php'; ?>
-
-<h1>404 - Not Found</h1>
-<p>The page you requested could not be found.</p>
-<p><a href="/">Go back home</a></p>
-
-<?php require __DIR__ . '/layout/footer.php'; ?>
